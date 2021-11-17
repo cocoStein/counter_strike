aa58e3cf9d9e75d35d7d391c2a0fcbd3495ab885
TheoSchlaeppi <Schlaeppitheo@outlook.com>
2021-11-17 15:24:27 +0100

    e
    e

---
+++ case.py
@@ -0,0 +1,7 @@
+class Case:
+    def __init__(self, x, y, valeur = False):
+        self.x = x #Coor horizontal
+        self.y = y #Coor vertical
+        self.valeur = valeur    #Bombe ou pas
+    
+    
--- plato.py
+++ plato.py
@@ -1,3 +1,19 @@
+from case import *
+
+
 class Plateau:
-    def __init__(self, ):
-je suis bo
+    def __init__(self, list = []):
+        self.list = list
+
+    def matrice(self, x, y):
+        for i in range(y):
+            k = []
+            for n in range(x):
+                k.append(case(n,i))
+            list.append(k)
+            k.clear()
+
+
+zeg = Plateau()
+zeg.matrice(8,3)
+print(zeg.list)
