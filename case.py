d4ae76c667573ec240aba84f84faf86a9207cc1d
TheoSchlaeppi <Schlaeppitheo@outlook.com>
2021-11-17 16:10:57 +0100

    avancwement

--- case.py
+++ case.py
@@ -1,14 +1,42 @@
 class Case:
-    def __init__(self, x, y, valeur = False):
+    def __init__(self, x, y, valeur = False, numero = 0):
         self.x = x #Coor horizontal
         self.y = y #Coor vertical
         self.valeur = valeur    #Bombe ou pas
+        self.numero = numero
     
     def __print__(self):
        return self.valeur
 
-    
+    def check(self, list):
 
+        #check en haut 
+        if list[self.y] == 0:
+            pass
+        else:
+            if list[self.y - 1][self.x - 1].valeur == True:
+                self.numero += 1
+            if list[self.y - 1][self.x].valeur == True:
+                self.numero += 1
+            if list[self.y - 1][self.x + 1].valeur == True:
+                self.numero += 1
+                
+        #check cotés
+        if list[self.y - 1][self.x - 1].valeur == True:
+                self.numero += 1
+        if list[self.y - 1][self.x - 1].valeur == True:
+                self.numero += 1
+
+        #check en bas 
+        if list[self.y] == 0:
+            pass
+        else:
+            if list[self.y - 1][self.x - 1].valeur == True:
+                self.numero += 1
+            if list[self.y - 1][self.x].valeur == True:
+                self.numero += 1
+            if list[self.y - 1][self.x + 1].valeur == True:
+                self.numero += 1
 
 if __name__== "main":
     print("test")
