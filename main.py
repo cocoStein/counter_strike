ce8da039d03fb9a64da94a08b60575467567fc4f
cocoStein <79094799+cocoStein@users.noreply.github.com>
2021-11-17 15:06:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from typing import DefaultDict
+from typing_extensions import _AnnotatedAlias
 
+
+main = 34 / 7
+print(main) 
+AssertionErrora
+setaS
+_AnnotatedAliasa
+StopAsyncIterationa
+setattrS
+assert
+
+setattrd
+DefaultDict
