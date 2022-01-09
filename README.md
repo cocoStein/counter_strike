f2381cd94c606a2e1dce6070b6a200b2c7cd4993
corentinsteinhauser <costein972@gmail.com>
2022-01-09 15:17:28 +0100

    finale

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # counter_strike
-Démineur
+    Démineur - Projet OC informatique Gymnase de chamblandes - 2022
 
-Projet de travail pour l'oc informatique au Gymnase de Chamblandes
-THEO EST PAS BO
+##Auteurs
+Corentin Steinhauser
+Theo Schlaepppi
+Paul Kienner
+
+##Fichiers
+    *README.md
+    *LICENSE
+    *Journal_De_Bord_Demineur.ipynb
+    *case.py
+    *plato.py
+    *main.py
+    *gui.py
+
+##Démarrage du jeux
+Pour lancer le programme, il faut lancer le fichier main.py. Une fois lancer, il suffit de suivre les indications dans le terminal en observant soit sur la fenêtre Turtle soit dans le terminal les cases restantes et découvertes.
--- gui.py
+++ gui.py
@@ -84,7 +84,7 @@ def draw_number(platard, x, y):
 
 
     stuck1.pu()
-    stuck1.goto((x0 + x * x_space + x_space), (y0 - y * y_space - 2 * y_space))
+    stuck1.goto((x0 + x * x_space + x_space + 1/5 * x_space), (y0 - y * y_space - 2 * y_space))
     stuck1.pd()
     stuck1.write(platard.list[y][x].numero, font=("arial", int(x_space / 1.5), "normal"))
 
@@ -103,6 +103,6 @@ def draw_evidence(platard, x, y):
 
 
     stuck1.pu()
-    stuck1.goto((x0 + x * x_space - (2 / 3) * x_space) , (y0 - y * y_space + y_space))
+    stuck1.goto((x0 + x * x_space + x_space + 1/5 * x_space), (y0 - y * y_space - 2 * y_space))
     stuck1.pd()
     stuck1.write("!", font=("arial", int(x_space / 1.5), "normal"))
